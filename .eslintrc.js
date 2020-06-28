3272e85ff2bcbf3c258c3c1ef7ab37d0c94dd71f
José Gilberto <medeiros.gilberto.br@gmail.com>
2020-06-28 03:37:08 -0300

    Add prettier to the .eslintrc file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,16 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: [
+    "@typescript-eslint",
+    "prettier"
+  ],
+  rules: {
+    "prettier/prettier": "error"
+  },
   extends: [
     "eslint:recommended",
+    "plugin:prettier/recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended"
   ],
