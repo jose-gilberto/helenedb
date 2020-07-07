cbfec401d376b35d9daac7f9add8c4fa21010e27
José Gilberto <medeiros.gilberto.br@gmail.com>
2020-07-07 00:07:52 -0300

    Creating file manager class

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@
 import Lexer from './query/processor/lexer/Lexer';
 import SymbolTable from './query/processor/symbol-table/SymbolTable';
 import TokenType from './query/processor/lexer/token/TokenType';
+import FileManager from './data/file/FileManager';
 
 const program = `
 CREATE SCHEMA public AUTHORIZATION user; 22.6 33 ( 22.3 ) + 3 - 4 *
@@ -13,3 +14,5 @@ const lexer = new Lexer(sb);
 const tokens = lexer.start(program);
 
 console.log(tokens);
+
+new FileManager('');
