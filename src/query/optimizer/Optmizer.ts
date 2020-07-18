7552578504387f8535477322464c4f08289561f0
José Gilberto <medeiros.gilberto.br@gmail.com>
2020-07-18 02:23:38 -0300

    Creating Optimizer class

---
+++ src/query/optimizer/Optmizer.ts
@@ -0,0 +1,10 @@
+import AlgebraTree from '../processor/parser/algebra/structures/AlgebraTree';
+
+export default class Optimizer {
+  public static optmizeQuery(): void {
+    // Step 1 - Split the selection and execute then faster than possible
+    // Step 2 -
+    // Step 3 - Change cartesian products with selections after them by joins
+    // Step 4 - Execute projections faster than possible
+  }
+}
