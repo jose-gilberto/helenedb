1e0cda7cbf6fc045137bbe1170e39a2708d7f189
José Gilberto <medeiros.gilberto.br@gmail.com>
2020-08-21 22:53:42 -0300

    Updating tokenizer method in lexer

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,14 @@
 // This is a test file, do not leave them to production
-import Lexer from './query/processor/lexer/Lexer';
-import SymbolTable from './query/processor/symbol-table/SymbolTable';
-import TokenType from './query/processor/lexer/token/TokenType';
-import Parser from './query/processor/parser/Parser';
-import QueryAlgebra from './query/processor/parser/algebra/QueryAlgebra';
-import Optimizer from './query/optimizer/Optmizer';
 
-// const program = `
-// SELECT employee.lname
-// FROM employee, works_on, project
-// WHERE employee.pname = 'Aquarius' AND project.pnumber = works_on.pno AND employee.ssn = works_on.ssn AND project.pdate > '2020-10-03';
-// `;
+// import Lexer from './query/processor/lexer/Lexer';
+// import TokenType from './query/processor/lexer/token/TokenType';
 
-// const program = `SELECT table.id FROM table WHERE table.id = '1' AND table.name = 'João';`;
+// const program = `1 + 2 + (3)`;
+// const lexer = new Lexer(program);
 
-const program = `
-SELECT product.name
-FROM product, category
-WHERE product.id_category = category.id AND product.value > 50.00;
-`;
-
-const sb = new SymbolTable();
-
-const lexer = new Lexer();
-const tokens = lexer.start(program);
-
-console.log(JSON.stringify(tokens));
-
-const parser = new Parser(tokens, sb);
-
-const tree = parser.getTree();
-
-// Optimizer.optmizeQuery(tree, sb)
-// tree.postOrder(tree.getRoot())
-// console.log(sb);
-
-const qAlgebra = parser.getData();
-qAlgebra.optimizeTree();
+// while (true) {
+//   let token = lexer.nextToken();
+//   if (token.getType() === TokenType.EOF)
+//     break;
+//   console.log(`<${token.getType()}, ${token.getValue()}>`)
+// }
--- src/query/processor/lexer/Lexer.ts
+++ src/query/processor/lexer/Lexer.ts
@@ -1,519 +1,107 @@
 import TokenType from './token/TokenType';
 import Token from './token/Token';
-import LexicalError from './LexicalError';
 
 export default class Lexer {
-  public static readonly KEYWORDS: TokenType[] = [
-    TokenType.ADD,
-    TokenType.ALL,
-    TokenType.ALTER,
-    TokenType.AND,
-    TokenType.AS,
-    TokenType.ASC,
-    TokenType.AUTHORIZATION,
-    TokenType.BETWEEN,
-    TokenType.BY,
-    TokenType.COLUMN,
-    TokenType.CONSTRAINT,
-    TokenType.CREATE,
-    TokenType.DELETE,
-    TokenType.DESC,
-    TokenType.DISTINCT,
-    TokenType.DROP,
-    TokenType.EXISTS,
-    TokenType.EXPLAIN,
-    TokenType.FOREIGN,
-    TokenType.FROM,
-    TokenType.GROUP,
-    TokenType.HAVING,
-    TokenType.IN,
-    TokenType.INCLUDE,
-    TokenType.INDEX,
-    TokenType.INSERT,
-    TokenType.INTO,
-    TokenType.KEY,
-    TokenType.LIKE,
-    TokenType.LIMIT,
-    TokenType.NOT,
-    TokenType.NULL,
-    TokenType.ON,
-    TokenType.OR,
-    TokenType.ORDER,
-    TokenType.PRIMARY,
-    TokenType.SELECT,
-    TokenType.SET,
-    TokenType.TABLE,
-    TokenType.UPDATE,
-    TokenType.SCHEMA,
-    TokenType.VALUES,
-    TokenType.VIEW,
-    TokenType.WHERE,
-  ];
+  private program: string;
+  private position: number;
 
-  public static readonly DATATYPES = [
-    TokenType.INTEGER,
-    TokenType.BOOL,
-    TokenType.DATE,
-    TokenType.FLOAT,
-    TokenType.TEXT,
-  ];
+  // Flow Control Variables
+  // Line pointer
+  //private lineCounter: number;
+  // Column pointer
+  //private colCounter: number;
 
-  private tokens: Token[];
-  private programCounter: number;
-  private lineCounter: number; // TODO: add suport
-  private colCounter: number; // TODO: add suport
-
-  constructor() {
-    this.tokens = [];
-    this.lineCounter = 1;
-    this.programCounter = 0;
-    this.colCounter = 0;
+  constructor(program: string) {
+    this.program = program;
+    this.position = 0;
   }
 
-  public numberAutomata(sourceCode: string): Token {
-    let state = 0,
-      lexem = '';
-    while (this.programCounter < sourceCode.length || state === 3) {
-      const currChar = sourceCode[this.programCounter];
-      switch (state) {
-        case 0:
-          if (currChar.match(/[0-9]/)) {
-            state = 1;
-            this.programCounter++;
-            this.colCounter++;
-            lexem += currChar;
-            continue;
-          } else {
-            throw new LexicalError('Lexical Error');
-          }
-        case 1:
-          if (currChar === '.') {
-            state = 2;
-            this.programCounter++;
-            this.colCounter++;
-            lexem += currChar;
-            continue;
-          } else if (currChar === undefined || !currChar.match(/[0-9]/)) {
-            state = 3;
-            continue;
-          } else {
-            state = 1;
-            this.programCounter++;
-            this.colCounter++;
-            lexem += currChar;
-            continue;
-          }
-        case 2:
-          if (currChar === undefined || !currChar.match(/[0-9]/)) {
-            state = 3;
-            continue;
-          } else {
-            state = 2;
-            this.programCounter++;
-            this.colCounter++;
-            lexem += currChar;
-            continue;
-          }
-        case 3:
-          return new Token(
-            TokenType.NUMBER_LITERAL,
-            Number(lexem),
-            this.colCounter - lexem.length,
-            this.lineCounter
-          );
-      }
+  /**
+   * @description Returns the current character marked with the pointer.
+   * @returns The current char.
+   */
+  private current(): string {
+    if (this.position >= this.program.length) return '\0';
+    return this.program[this.position];
+  }
+
+  private next(): void {
+    this.position++;
+  }
+
+  public nextToken(): Token {
+    // symbols
+
+    if (this.position >= this.program.length) {
+      return new Token(TokenType.EOF, '\0', 0, 0);
     }
-    throw new Error('Stack overflow');
-  }
 
-  public textDateAutomata(sourceCode: string): Token {
-    let state = 0,
-      lexem = '';
-    while (this.programCounter < sourceCode.length || state == 2) {
-      const currChar = sourceCode[this.programCounter];
-      switch (state) {
-        case 0:
-          if (currChar === "'") {
-            state = 1;
-            this.programCounter++;
-            this.colCounter++;
-            continue;
-          } else {
-            throw new LexicalError('Lexical Error');
-          }
-        case 1:
-          if (currChar === "'") {
-            state = 2;
-            this.programCounter++;
-            this.colCounter++;
-            continue;
-          } else {
-            state = 1;
-            this.programCounter++;
-            this.colCounter++;
-            lexem += currChar;
-            continue;
-          }
-        case 2:
-          if (
-            lexem.match(/^\d{4}-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])$/)
-          ) {
-            return new Token(
-              TokenType.DATE_LITERAL,
-              lexem,
-              this.colCounter - lexem.length,
-              this.lineCounter
-            );
-          }
-          return new Token(
-            TokenType.TEXT_LITERAL,
-            lexem,
-            this.colCounter - lexem.length,
-            this.lineCounter
-          );
-      }
+    if (this.current().match(/[0-9]/)) {
+      const start = this.position;
+
+      while (this.current().match(/[0-9]/)) this.next();
+
+      const length = this.position - start;
+      const lexem = this.program.substring(start, start + length);
+      // const value = parseInt(lexem)
+
+      // TODO: add columns and rows support
+      return new Token(TokenType.NUMBER_LITERAL, lexem, 0, 0);
     }
-    throw new Error('Stack overflow');
-  }
 
-  public identifierAutomata(sourceCode: string): Token {
-    let state = 0,
-      lexem = '';
-    while (this.programCounter < sourceCode.length) {
-      const currChar = sourceCode[this.programCounter];
-      switch (state) {
-        case 0:
-          if (currChar == undefined || !currChar.match(/[_a-zA-Z]/)) {
-            state = 1;
-            continue;
-          } else {
-            state = 0;
-            lexem += currChar;
-            this.colCounter++;
-            this.programCounter++;
-            continue;
-          }
-        case 1:
-          if (Lexer.KEYWORDS.includes(lexem.toUpperCase() as TokenType)) {
-            // TODO: implements token line and col
-            const tType = lexem.toUpperCase() as TokenType;
-            return new Token(
-              tType,
-              '',
-              this.colCounter - lexem.length,
-              this.lineCounter
-            );
-          }
+    if (this.current().match(/\s/)) {
+      const start = this.position;
 
-          // Support Data Types
-          if (Lexer.DATATYPES.includes(lexem.toUpperCase() as TokenType)) {
-            const tType = lexem.toUpperCase() as TokenType;
-            return new Token(
-              tType,
-              '',
-              this.colCounter - lexem.length,
-              this.lineCounter
-            );
-          }
+      while (this.current().match(/\s/)) this.next();
 
-          // TODO: implements token to receive lexem and addr
-          return new Token(
-            TokenType.IDENTIFIER,
-            lexem,
-            this.colCounter - lexem.length,
-            this.lineCounter
-          );
-      }
+      const length = this.position - start;
+      const lexem = this.program.substring(start, start + length);
+
+      // TODO: add columns and rows support
+      return new Token(TokenType.WHITESPACE, lexem, 0, 0);
     }
-    throw new Error('Stack overflow');
-  }
 
-  public symbolsAutomata(sourceCode: string): Token {
-    let state = 0;
-    while (this.programCounter < sourceCode.length || state != 0) {
-      const currChar = sourceCode[this.programCounter];
-      switch (state) {
-        case 0:
-          if (currChar == ';') {
-            state = 1;
-            this.programCounter++;
-            this.colCounter++;
-            continue;
-          } else if (currChar == '.') {
-            state = 2;
-            this.programCounter++;
-            this.colCounter++;
-          } else if (currChar == ',') {
-            state = 3;
-            this.programCounter++;
-            this.colCounter++;
-          } else {
-            throw new LexicalError('Not suported yet');
-          }
-          break;
-        case 1:
-          return new Token(
-            TokenType.SEMICOLON,
-            '',
-            this.colCounter - 1,
-            this.lineCounter
-          );
-        case 2:
-          return new Token(
-            TokenType.DOT,
-            '',
-            this.colCounter - 1,
-            this.lineCounter
-          );
-        case 3:
-          return new Token(
-            TokenType.COMMA,
-            '',
-            this.colCounter - 1,
-            this.lineCounter
-          );
-      }
+    if (this.current() === '+') {
+      // TODO: add columns and rows support
+      this.next();
+      return new Token(TokenType.PLUS, '+', 0, 0);
     }
-    throw new Error('Stack overflow');
-  }
 
-  public separatorsAutomata(sourceCode: string): Token {
-    // [ ] ( )
-    const state = 0;
-    while (this.programCounter < sourceCode.length) {
-      const currChar = sourceCode[this.programCounter];
-      switch (state) {
-        case 0:
-          if (currChar == '(') {
-            this.programCounter++;
-            this.colCounter++;
-            return new Token(
-              TokenType.LPAR,
-              '',
-              this.colCounter - 1,
-              this.lineCounter
-            );
-          } else if (currChar == ')') {
-            this.programCounter++;
-            this.colCounter++;
-            return new Token(
-              TokenType.RPAR,
-              '',
-              this.colCounter - 1,
-              this.lineCounter
-            );
-          } else if (currChar == '[') {
-            this.programCounter++;
-            this.colCounter++;
-            return new Token(
-              TokenType.LBRC,
-              '',
-              this.colCounter - 1,
-              this.lineCounter
-            );
-          } else if (currChar == ']') {
-            this.programCounter++;
-            this.colCounter++;
-            return new Token(
-              TokenType.RBRC,
-              '',
-              this.colCounter - 1,
-              this.lineCounter
-            );
-          } else {
-            throw new LexicalError('Lexical Error');
-          }
-      }
+    if (this.current() === '-') {
+      // TODO: add columns and rows support
+      this.next();
+      return new Token(TokenType.MINUS, '-', 0, 0);
     }
-    throw new Error('Stack overflow');
-  }
 
-  public operatorsAutomata(sourceCode: string): Token {
-    // + * - /
-    const state = 0;
-    while (this.programCounter < sourceCode.length) {
-      const currChar = sourceCode[this.programCounter];
-      switch (state) {
-        case 0:
-          if (currChar === '+') {
-            this.programCounter++;
-            this.colCounter++;
-            return new Token(
-              TokenType.PLUS,
-              '',
-              this.colCounter - 1,
-              this.lineCounter
-            );
-          } else if (currChar === '-') {
-            this.programCounter++;
-            this.colCounter++;
-            return new Token(
-              TokenType.MINUS,
-              '',
-              this.colCounter - 1,
-              this.lineCounter
-            );
-          } else if (currChar === '/') {
-            this.programCounter++;
-            this.colCounter++;
-            return new Token(
-              TokenType.DIV,
-              '',
-              this.colCounter - 1,
-              this.lineCounter
-            );
-          } else if (currChar === '*') {
-            this.programCounter++;
-            this.colCounter++;
-            return new Token(
-              TokenType.MULT,
-              '',
-              this.colCounter - 1,
-              this.lineCounter
-            );
-          } else {
-            throw new LexicalError('Lexical Error');
-          }
-      }
+    if (this.current() === '/') {
+      // TODO: add columns and rows support
+      this.next();
+      return new Token(TokenType.SLASH, '/', 0, 0);
     }
-    throw new Error('Stack overflow');
-  }
 
-  public relopAutomata(sourceCode: string): Token {
-    // < <= >= = != <>
-    let state = 0;
-    while (this.programCounter < sourceCode.length) {
-      const currChar = sourceCode[this.programCounter];
-      switch (state) {
-        case 0:
-          if (currChar === '=') {
-            state = 1;
-            this.programCounter++;
-            this.colCounter++;
-            continue;
-          } else if (currChar === '<') {
-            state = 2;
-            this.programCounter++;
-            this.colCounter++;
-            continue;
-          } else if (currChar === '>') {
-            state = 3;
-            this.programCounter++;
-            this.colCounter++;
-            continue;
-          } else if (currChar === '!') {
-            state = 4;
-            this.programCounter++;
-            this.colCounter++;
-            continue;
-          } else {
-            throw new LexicalError('Lexical Error');
-          }
-        case 1:
-          return new Token(
-            TokenType.EQ,
-            '',
-            this.colCounter - 1,
-            this.lineCounter
-          );
-        case 2:
-          if (currChar === '=') {
-            this.programCounter++;
-            this.colCounter++;
-            return new Token(
-              TokenType.LEQ,
-              '',
-              this.colCounter - 2,
-              this.lineCounter
-            );
-          } else {
-            return new Token(
-              TokenType.LESS,
-              '',
-              this.colCounter - 1,
-              this.lineCounter
-            );
-          }
-        case 3:
-          if (currChar === '=') {
-            this.programCounter++;
-            this.colCounter++;
-            return new Token(
-              TokenType.GEQ,
-              '',
-              this.colCounter - 2,
-              this.lineCounter
-            );
-          } else {
-            return new Token(
-              TokenType.GRT,
-              '',
-              this.colCounter - 1,
-              this.lineCounter
-            );
-          }
-        case 4:
-          if (currChar === '=') {
-            this.programCounter++;
-            this.colCounter++;
-            return new Token(
-              TokenType.DIFF,
-              '',
-              this.colCounter - 2,
-              this.lineCounter
-            );
-          } else {
-            throw new LexicalError('Lexical Error');
-          }
-      }
+    if (this.current() === '*') {
+      // TODO: add columns and rows support
+      this.next();
+      return new Token(TokenType.STAR, '*', 0, 0);
     }
-    throw new Error('Stack overflow');
-  }
 
-  public start(sourceCode: string): Token[] {
-    this.scanner(sourceCode);
-    return this.tokens;
-  }
-
-  public scanner(sourceCode: string): void {
-    while (this.programCounter < sourceCode.length) {
-      const currChar = sourceCode[this.programCounter];
-
-      if (currChar == ' ' || currChar == '\t') {
-        // spaces or tabs
-        this.programCounter++;
-        this.colCounter++;
-        continue;
-      } else if (currChar == '\r' || currChar == '\n') {
-        // line breakers
-        this.programCounter++;
-        this.lineCounter++;
-        this.colCounter = 0;
-        continue;
-      } else if (currChar.match(/[_a-zA-Z]/)) {
-        // identifier, keywords or datatypes
-        this.tokens.push(this.identifierAutomata(sourceCode));
-      } else if (currChar.match(/[.,;]/)) {
-        // symbols like: . ,
-        this.tokens.push(this.symbolsAutomata(sourceCode));
-      } else if (currChar.match(/[0-9]/)) {
-        // numbers
-        this.tokens.push(this.numberAutomata(sourceCode));
-      } else if (currChar.match(/[()]/)) {
-        // separators like: ( )
-        this.tokens.push(this.separatorsAutomata(sourceCode));
-      } else if (currChar.match(/[+-/*]/)) {
-        // operators like: + - / *
-        this.tokens.push(this.operatorsAutomata(sourceCode));
-      } else if (currChar == "'") {
-        // text or data
-        this.tokens.push(this.textDateAutomata(sourceCode));
-      } else if (currChar.match(/[=><!]/)) {
-        // relational operators
-        this.tokens.push(this.relopAutomata(sourceCode));
-      } else {
-        throw new LexicalError('Lexical Error');
-      }
+    if (this.current() === '(') {
+      // TODO: add columns and rows support
+      this.next();
+      return new Token(TokenType.LPAR, '(', 0, 0);
     }
+
+    if (this.current() === ')') {
+      // TODO: add columns and rows support
+      this.next();
+      return new Token(TokenType.RPAR, ')', 0, 0);
+    }
+
+    return new Token(
+      TokenType.BAD_TOKEN,
+      this.program[this.position - 1],
+      0,
+      0
+    );
   }
 }
